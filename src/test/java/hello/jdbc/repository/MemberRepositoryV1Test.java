af89bf574606b317fad29ceb63ab54f7582b8d1c
hyeonwoo <escape0619@gmail.com>
2022-05-11 23:49:10 +0900

    DataSource 적용(MemberRepositoryV1.java)

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,57 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach(){
+        //기본 DriverManager - 항상 새로운 커넥션을 획득.
+        //DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        //Connection Pooling
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        // save
+        Member memberV0 = new Member("memberV100", 10000);
+        repository.save(memberV0);
+
+        // findById
+        Member findMember = repository.findById(memberV0.getMemberId());
+        log.info("findMember={}", findMember);
+        Assertions.assertThat(findMember).isEqualTo(memberV0);
+
+        //update : money 10000 -> 20000
+        repository.update(memberV0.getMemberId(), 20000);
+        Member updatedMember = repository.findById(memberV0.getMemberId());
+        Assertions.assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(memberV0.getMemberId());
+        Assertions.assertThatThrownBy(() ->
+                repository.findById(memberV0.getMemberId())).isInstanceOf(NoSuchElementException.class);
+
+    }
+
+}
