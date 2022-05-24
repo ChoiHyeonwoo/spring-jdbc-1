2fb1cc56af27d7bee4ddc42f13a76f28b9fcfb2d
HyeonWoo Choi <escape0619@gmail.com>
2022-05-24 17:56:10 +0900

    printStackTrace (throw new RuntimeSQLException(e) -> 꼭 root cause를 포함해라.)

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
@@ -15,6 +17,15 @@ public class UnCheckedAppTest {
                 .isInstanceOf(Exception.class);
     }
 
+    @Test
+    void printEx(){
+        Controller controller = new Controller();
+        try{
+            controller.request();
+        }catch(Exception e){
+            log.info("ex", e);
+        }
+    }
     static class Controller{
         Service service = new Service();
 
